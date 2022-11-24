3a4438cdc6763b4cdd91beaaf8e199dceb688a3e
ShamimaNasrin <shamima.nasrion007@gmail.com>
2022-11-24 16:20:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.klfob8q.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(e => console.error(e));
+
+app.get('/', async (req, res) => {
+    res.send('Second Shelf surver running');
+})
+
+app.listen(port, () => {
+    console.log(`Second Shelf server running on port: ${port}`)
+})
